c3bc69f1c511c1039b61578c9fc40da497c4deb8
Elizabeth Su <elizabethsu@wirelessprv-10-193-172-44.near.illinois.edu>
2024-03-06 22:17:44 -0600

    test

--- street_signs/test.py
+++ street_signs/test.py
@@ -1,8 +1,9 @@
-from street_signs import Roboflow
+from test import Roboflow
 
 rf = Roboflow(api_key="rf_WBGHxj31NrTtNloPbJUvKDGLmQC3")
 project = rf.workspace().project("street signs")
 model = project.version(1).model
 
 # infer on a local image
-print(model.predict("road414.png", confidence=50, overlap=50).json())
+print(model.predict("road403.jpg", confidence=50, overlap=50).json())
+
